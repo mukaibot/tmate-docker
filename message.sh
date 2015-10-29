e62d0c273c99a8365c08826054a552530cedcbd0
Timothy Mukaibo <timothy@mukaibo.com>
2015-10-29 11:56:24 +1100

    Update logging output

--- message.sh
+++ message.sh
@@ -3,6 +3,7 @@
 DSA=`ssh-keygen -l -f /tmate-slave/keys/ssh_host_dsa_key 2>&1 | cut -d\  -f 2`
 RSA=`ssh-keygen -l -f /tmate-slave/keys/ssh_host_rsa_key 2>&1 | cut -d\  -f 2`
 ECDSA=`ssh-keygen -l -f /tmate-slave/keys/ssh_host_ecdsa_key 2>&1 | cut -d\  -f 2`
+HOSTNAME=`hostname -f`
 echo Add this to your ~/.tmate.conf file
 echo set -g tmate-server-host "<server ip/hostname>"
 echo set -g tmate-server-port \<server port\>
@@ -10,12 +11,12 @@ echo set -g tmate-server-rsa-fingerprint   \"$RSA\"
 echo set -g tmate-server-dsa-fingerprint   \"$DSA\"
 echo set -g tmate-server-ecdsa-fingerprint   \"$ECDSA\"
 echo set -g tmate-identity ""              # Can be specified to use a different SSH key.
-logger Add this to your ~/.tmate.conf file
-logger set -g tmate-server-host "<server ip/hostname>"
-logger set -g tmate-server-port \<server port\>
-logger set -g tmate-server-rsa-fingerprint   \"$RSA\"
-logger set -g tmate-server-dsa-fingerprint   \"$DSA\"
-logger set -g tmate-server-ecdsa-fingerprint   \"$ECDSA\"
-logger set -g tmate-identity ""              # Can be specified to use a different SSH key.
+logger "Add this to your ~/.tmate.conf file"
+logger "set -g tmate-server-host \"$HOSTNAME\""
+logger "set -g tmate-server-port 443"
+logger "set -g tmate-server-rsa-fingerprint   \"$RSA\""
+logger "set -g tmate-server-dsa-fingerprint   \"$DSA\""
+logger "set -g tmate-server-ecdsa-fingerprint   \"$ECDSA\""
+logger "set -g tmate-identity \"\""              # Can be specified to use a different SSH key.
 
 touch /tmp/message.sh
